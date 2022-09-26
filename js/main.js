97359a609791a922ee9e9e82ee14d5e31c63d548
hanahana87 <life.hana87ca@gmail.com>
2022-09-26 16:11:39 +0900

    original

---
+++ js/main.js
@@ -0,0 +1,55 @@
+$(function($){
+    $('.bg-swicher').bgSwitcher({
+        images: ['../images/logo1.png','../images/logo2.png','../images/logo1.png'],
+        interval: 3000,
+        loop: true
+    });
+
+    $(window).scroll(function (){
+        var targetElement = $('.fadein').offset().top;
+        var scroll = $(window).scrollTop();
+        var windowHeight = $(window).height();
+        if(scroll > targetElement - windowHeight + 200){
+            $('.fadein').css('opacity','1');
+            $('.fadein').css('transform', 'translateY(0)');
+        }
+    });
+
+    $('.slider').slick({
+        arrows: false,
+        autoplay: true,
+        dots: true,
+        centerMode: true,
+        centerPadding: '25%',
+    });
+
+    $('.more-button').on('click', function(){
+        if($('.open-box').is(':hidden')) {
+            $('.open-box').slideDown();
+            $(this).text('close');
+            $(this).css('background-color', '#808080');
+        } else {
+            $('.open-box').slideUp();
+            $(this).text('more');
+            $(this).css('background-color', '#fabb51');  
+        }
+
+    });
+
+    $(".modal-button").modaal();
+
+    $('nav a[href^="#"]').click(function() {
+        var adjust = 0;
+        var speed = 400;
+        var href = $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top - adjust;
+        $('body,html').animate({
+            scrollTop: position
+        }, speed,'swing');
+        return false;
+    });
+
+
+});
+
